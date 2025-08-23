8f3a1cb3293bf8e56a775337fa20691016e655a1
kiritogtsa <tenseishitarrakenseshita@gmail.com>
2025-08-22 21:51:33 -0300

    terminando os campos da estruturas

--- server/src/server/models/colaborador/colaborador.go
+++ server/src/server/models/colaborador/colaborador.go
@@ -1,6 +1,15 @@
 package colaborador
 
-type Colaborador struct{}
+import "github.com/Emanuelleprestes/InfoSmart-Solutions.git/server/models/cargo"
+
+type Colaborador struct {
+	id              int
+	nome            string
+	cpf             string
+	email_coportivo string
+	telefone        string
+	cargo           *cargo.Cargo
+}
 
 func Newcolaborador() *Colaborador {
 	return &Colaborador{}
--- server/src/server/models/derpartamento/derpartamento.go
+++ server/src/server/models/derpartamento/derpartamento.go
@@ -1,6 +1,14 @@
 package derpartamento
 
-type Derpartamento struct{}
+import "github.com/Emanuelleprestes/InfoSmart-Solutions.git/server/models/colaborador"
+
+type Derpartamento struct {
+	// não preciso colocar o nome correto por casua da
+	// da json alguma que me permite colocar o nome da tabela
+	id     int
+	nome   string
+	gestor *colaborador.Colaborador
+}
 
 func NewDerpartamento() *Derpartamento {
 	return &Derpartamento{}
